dba7d0b2f8c284a43d8ffe7e9a1eaa2f58e3fcb7
Martyr211 <ayush881gupta@gmail.com>
2022-06-06 12:25:54 +0530

    .

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,10 @@
-def call(){
-   def set_test = "tesing point of time"
-   sh "echo test 2 shared lib"
-   println "${set_test}"
-}
 // def call(){
-//    sh ' mkdir ./target'
-//    sh "echo 'docker-build-list.yml file  archieve test' > ./target/docker-build-list.yml"
-//    archiveArtifacts artifacts: 'target/docker-build-list.yml', fingerprint: true, allowEmptyArchive: true
+//    def set_test = "tesing point of time"
+//    sh "echo test 2 shared lib"
+//    println "${set_test}"
 // }
+def call(){
+   sh ' mkdir ./target'
+   sh "echo 'docker-build-list.yml file  archieve test' > ./target/docker-build-list.yml"
+   archiveArtifacts artifacts: 'target/docker-build-list.yml', fingerprint: true, allowEmptyArchive: true
+}
