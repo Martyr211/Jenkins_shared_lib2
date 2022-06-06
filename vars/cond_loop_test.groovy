5984b017521810921eb57e1d242a2d0ab6252e4c
Martyr211 <ayush881gupta@gmail.com>
2022-06-06 13:20:14 +0530

    new add func

--- vars/cond_loop_test.groovy
+++ vars/cond_loop_test.groovy
@@ -1,6 +1,10 @@
 def call (sample){
     if (sample) {
-        println("Conditional statement Worked")
+        test {
+         sh ' mkdir ./target'
+         sh "echo 'docker-build-list.yml file  archieve test' > ./target/docker-build-list.yml"
+         archiveArtifacts artifacts: 'target/docker-build-list.yml', fingerprint: true, allowEmptyArchive: true
+      }
     }
     else {
         println("testing failed")
--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,15 @@
-// def call(){
-//    def set_test = "tesing point of time"
-//    sh "echo test 2 shared lib"
-//    println "${set_test}"
-// }
-
 def call(){
-   if('True') {
-      test {
-         sh ' mkdir ./target'
-         sh "echo 'docker-build-list.yml file  archieve test' > ./target/docker-build-list.yml"
-         archiveArtifacts artifacts: 'target/docker-build-list.yml', fingerprint: true, allowEmptyArchive: true
-      }
-   }
+   def set_test = "tesing point of time"
+   sh "echo test 2 shared lib"
+   println "${set_test}"
 }
+
+// def call(){
+//    if ('True'){
+//       test {
+//          sh ' mkdir ./target'
+//          sh "echo 'docker-build-list.yml file  archieve test' > ./target/docker-build-list.yml"
+//          archiveArtifacts artifacts: 'target/docker-build-list.yml', fingerprint: true, allowEmptyArchive: true
+//       }
+//    }
+// }
